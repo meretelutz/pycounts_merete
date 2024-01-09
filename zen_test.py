a6a6f1452d51932bd24f702bb30ba3a424823de1
Merete Lutz <meretelutz@gmail.com>
2024-01-08 20:36:32 -0800

    build: remove upper bound on dependency versions

--- zen_test.py
+++ zen_test.py
@@ -1,2 +1,10 @@
 from pycounts_merete.pycounts_merete import count_words
 count_words("zen.txt")
+
+from pycounts_merete.pycounts_merete import count_words
+from pycounts_merete.plotting import plot_words
+counts = count_words("zen.txt")
+fig = plot_words(counts, 10)
+
+import matplotlib.pyplot as plt
+plt.show()
